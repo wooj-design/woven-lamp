3a4bfbc320a80932228e4bd4b805c93659fc7346
mj10021 <jamesthespeedy@gmail.com>
2023-02-15 12:17:24 -0500

    the 3mfs and gcode

--- scripts/seam_blend.py
+++ scripts/seam_blend.py
@@ -1,10 +1,11 @@
 import re
 import os
 import copy
+import math
 
 current_location = os.getcwd()
 file_name = r'/blend_test_weave_sr.gcode'
-file_location = current_location + file_name
+file_location = current_location + r'/../gcode' + file_name
 
 
 with open(file_location, 'r') as f:
@@ -20,21 +21,39 @@ for i in range(5, len(command_layer_list)-1):
     next_layer = copy.deepcopy(command_layer_list[i+1])
 
     next_z = re.match(r';Z:(.*)', next_layer[1])
+    if next_z is None:
+        print(i)
     next_z = [float(z_) for z_ in next_z.groups()]
     next_z = 'Z' + str(next_z[0]) + ' '
 
     last_command = (layer[::-1])[1]
+    second_to_last_command = (layer[::-1][2])
 
     if type(last_command) is not str:
         continue
 
     if re.match(r'M106', last_command):
-        print(f'{i} , {last_command}')
         last_command = (layer[::-1])[2]
+        second_to_last_command = (layer[::-1][3])
 
-    match = re.match(r'(G1) (X.*) (Y.*) (E.*)', last_command)
+    match = re.match(r'G1 (X.*) (Y.*) (E.*)', last_command)
+    prev_match = re.match(r'G1 (X.*) (Y.*) (E.*)', second_to_last_command)
 
-    if match is None:
+    dx = float(match.groups()[0][1:]) - float(prev_match.groups()[0][1:])
+    dy = float(match.groups()[1][1:]) - float(prev_match.groups()[1][1:])
+    de = float(match.groups()[2][1:]) - float(prev_match.groups()[2][1:])
+    
+    total_dist = math.sqrt(dx**2 + dy**2)
+    flow_rate = de/total_dist
+
+    dz = 0.5
+    new_dist = math.sqrt(dx**2 + dy**2 + dz**2)
+    new_flow = flow_rate * new_dist
+
+    print(de, new_flow)
+    print(total_dist, new_dist)
+
+    if match is None or prev_match is None:
         print(i)
         continue
     
@@ -46,9 +65,10 @@ for i in range(5, len(command_layer_list)-1):
         out_command += ' '
     out_command += '; modified command'
 
+
     command_layer_list[i][len(layer)-2] = out_command
 
 out_data = 'LAYER_CHANGE'.join(["\n".join(x) for x in command_layer_list])
 
-with open('test_output.gcode', 'w') as f:
-    f.write(out_data)
+#with open('test_output.gcode', 'w') as f:
+#    f.write(out_data)
